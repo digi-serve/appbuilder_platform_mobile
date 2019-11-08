008028726ef5617c51e6d81d96b981f61a1c3a7e
James Duncan <james.duncan@cru.org>
2019-11-08 11:22:04 +0700

    add background timeout as a reasource

--- init/initResources.js
+++ init/initResources.js
@@ -3,11 +3,15 @@
  * Setup the main application Pages
  */
 import analytics from "../resources/Analytics.js";
+import backgroundTimeout from "../resources/BackgroundTimeout.js";
 import notifications from "../resources/Notifications.js";
+
 // Just including the Storage module makes sure it is initialized early
 // in the app startup.
 /* eslint-disable-next-line no-unused-vars */
 import { storage } from "../resources/Storage.js";
+
+var config = require("../../config/config.js");
 
 export default {
     init: () => {
@@ -30,6 +34,10 @@ export default {
                 "Error starting notifications system"
             );
         }
+
+        if (config.backgroundTimeout.enabled)
+            backgroundTimeout(config.backgroundTimeout.duration);
+
         return Promise.resolve(); // nothing async, so just return
     }
 };
---
+++ resources/BackgroundTimeout.js
@@ -0,0 +1,52 @@
+/**
+ * Reload the app if it goes into the background for too long.
+ */
+"use strict";
+
+import moment from "moment";
+
+var pauseTime;
+
+export default function BackgroundTimeout(minutesToWait = 9) {
+    // Listen for when the app goes into the background and store a timestamp
+    document.addEventListener(
+        "pause",
+        () => {
+            pauseTime = moment();
+            console.log("Paused: ", pauseTime);
+        },
+        false
+    );
+
+    // Listen for when the app goes into the background and store a timestamp
+    document.addEventListener(
+        "resume",
+        () => {
+            setTimeout(function() {
+                var now = moment();
+                console.log("Resume: ", now);
+                if (moment.isMoment(pauseTime)) {
+                    var minPassed = now.diff(pauseTime, "minutes");
+                    console.log("Time Lapse:", minPassed, " minutes");
+                    if (minPassed > minutesToWait) {
+                        var node = document.createElement("DIV");
+                        node.id = "lockFade";
+                        node.classList.add("lockFade");
+                        document
+                            .getElementsByTagName("BODY")[0]
+                            .appendChild(node);
+                        setTimeout(function() {
+                            document
+                                .getElementById("lockFade")
+                                .classList.add("lockFadeIn");
+                            setTimeout(function() {
+                                window.location.reload();
+                            }, 600);
+                        }, 50);
+                    }
+                }
+            }, 0);
+        },
+        false
+    );
+}
