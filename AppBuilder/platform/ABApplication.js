0090759112e326c5be96f52f5d8595b0ed8475a8
James Duncan <james.duncan@cru.org>
2020-07-31 15:44:44 +0700

    add objectsAll function

--- AppBuilder/platform/ABApplication.js
+++ AppBuilder/platform/ABApplication.js
@@ -13,43 +13,47 @@ var moment = require("moment");
 var uuidv4 = require("uuid/v4");
 
 module.exports = class ABApplication extends ABApplicationCore {
-    constructor(attributes) {
-        super(attributes);
-    }
+   constructor(attributes) {
+      super(attributes);
+   }
 
-    findDC(id) {
-        return this.datacollections((d) => {
-            return d.id == id;
-        })[0];
-    }
+   findDC(id) {
+      return this.datacollections((d) => {
+         return d.id == id;
+      })[0];
+   }
 
-    cloneDeep(obj) {
-        // lodash is available on the platform
-        return _.cloneDeep(obj);
-    }
+   cloneDeep(obj) {
+      // lodash is available on the platform
+      return _.cloneDeep(obj);
+   }
 
-    languageDefault() {
-        var lang = navigator.language || "en";
-        if (lang.indexOf("en-") != -1) lang = "en";
-        if (lang.indexOf("zh-") != -1) lang = "zh-hans"; // show one version of Chinese for all
-        if (lang.indexOf("ko-") != -1) lang = "zh-hans"; // show Chinese instead of Korean
-        return lang;
-    }
+   languageDefault() {
+      var lang = navigator.language || "en";
+      if (lang.indexOf("en-") != -1) lang = "en";
+      if (lang.indexOf("zh-") != -1) lang = "zh-hans"; // show one version of Chinese for all
+      if (lang.indexOf("ko-") != -1) lang = "zh-hans"; // show Chinese instead of Korean
+      return lang;
+   }
 
-    uuid() {
-        return uuidv4();
-    }
+   uuid() {
+      return uuidv4();
+   }
 
-    createdAt(date) {
-        return this.sqlDateTime(date);
-    }
+   createdAt(date) {
+      return this.sqlDateTime(date);
+   }
 
-    updatedAt(date) {
-        return this.sqlDateTime(date);
-    }
+   objectsAll() {
+      return webix.storage.local.get("_ABObjects");
+   }
 
-    sqlDateTime(date) {
-        // taken from app_builder/api/services/AppBuilder.rules.toSQLDateTime()
-        return moment(date).format("YYYY-MM-DD HH:mm:ss");
-    }
+   updatedAt(date) {
+      return this.sqlDateTime(date);
+   }
+
+   sqlDateTime(date) {
+      // taken from app_builder/api/services/AppBuilder.rules.toSQLDateTime()
+      return moment(date).format("YYYY-MM-DD HH:mm:ss");
+   }
 };
