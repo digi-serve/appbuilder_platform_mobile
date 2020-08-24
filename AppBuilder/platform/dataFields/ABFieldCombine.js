0517467720d564376cf19da1e116e6e0ba3f33d0
James Duncan <james.duncan@cru.org>
2020-08-24 12:02:32 +0700

    add ABFieldCombine to platform dataFields

---
+++ AppBuilder/platform/dataFields/ABFieldCombine.js
@@ -0,0 +1,14 @@
+/**
+ * ABFieldCombine
+ *
+ * This is the platform dependent implementation of ABFieldCombine.
+ *
+ */
+
+var ABFieldCombineCore = require("../../core/dataFields/ABFieldCombineCore");
+
+module.exports = class ABFieldCombine extends ABFieldCombineCore {
+   constructor(attributes, object) {
+      super(attributes, object);
+   }
+};
