331be159f4dd28f4406cf6652bb274b8b16c0d98
James Duncan <jduncandesign@gmail.com>
2021-03-08 15:42:14 +0700

    put back target width and height

--- resources/CameraPlatform.js
+++ resources/CameraPlatform.js
@@ -102,9 +102,9 @@ class CameraPlatform extends EventEmitter {
                encodingType: window.Camera.EncodingType.JPEG,
                mediaType: window.Camera.MediaType.PICTURE,
                sourceType: window.Camera.PictureSourceType.CAMERA,
-               correctOrientation: true
-               // targetWidth: width,
-               // targetHeight: height
+               correctOrientation: true,
+               targetWidth: width,
+               targetHeight: height
             }
          );
       });
@@ -144,9 +144,9 @@ class CameraPlatform extends EventEmitter {
                allowEdit: canEditPhoto,
                encodingType: window.Camera.EncodingType.JPEG,
                mediaType: window.Camera.MediaType.PICTURE,
-               sourceType: window.Camera.PictureSourceType.SAVEDPHOTOALBUM
-               // targetWidth: width,
-               // targetHeight: height
+               sourceType: window.Camera.PictureSourceType.SAVEDPHOTOALBUM,
+               targetWidth: width,
+               targetHeight: height
             }
          );
       });
