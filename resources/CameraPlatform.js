96c28ee07657c9330e76f762baab886c6401e80a
James Duncan <jduncandesign@gmail.com>
2021-09-13 14:13:16 +0700

    try cache directory for storage

--- resources/CameraPlatform.js
+++ resources/CameraPlatform.js
@@ -64,7 +64,7 @@ class CameraPlatform extends EventEmitter {
          () => {
             this.camera = navigator.camera;
             window.resolveLocalFileSystemURL(
-               cordova.file.tempDirectory,
+               cordova.file.cacheDirectory,
                (_directoryEntry) => {
                   this.directoryEntry = _directoryEntry;
                   this.emit("ready");
