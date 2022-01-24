31a5963cc0d2292920ccd912a66c37d708a4dca3
Joshua Chan <joshua@appdevdesigns.net>
2022-01-24 01:08:59 -0800

    Camera access and photo file storage using PWA instead of Cordova

---
+++ resources/CameraPWA.js
@@ -0,0 +1,510 @@
+/**
+ * @class CameraPWA
+ *
+ * Manages taking photos with the device's camera, and saving them to the
+ * app's data directory.
+ *
+ * You can use getCameraPhoto() or getLibraryPhoto() to obtain an image file.
+ *
+ * Previously, it was possible to use the permanent `cdvfile` value of that
+ * image to display it in the DOM (inside an <img> tag). This is no longer
+ * possible in iOS. Now only the temporary `url` value can be displayed.
+ *
+ * Because it is temporary, the `url` cannot be reliably stored for future use.
+ * Instead, store the image filename. Then later when you need to display the
+ * image, use tempUrl(filename) to obtain a fresh URL.
+ *
+ * Exports a singleton instance.
+ */
+"use strict";
+
+import async from "async";
+import EventEmitter from "eventemitter2";
+import Log from "./Log";
+import uuid from "uuid/v1";
+
+import { storage } from "./Storage.js";
+import fileStorage from "./FileStorage.js";
+
+const defaultHeight = 2000;
+const defaultWidth = 2000;
+
+class CameraPWA extends EventEmitter {
+   constructor() {
+      super();
+
+      // backend HTML elements
+      this._$backend = null;
+      this._$input = null;
+      this._$reset = null;
+
+      this.init();
+   }
+
+
+   /**
+    * Internal function to trigger the device camera and deliver the image
+    * file.
+    *
+    * @param {string} type
+    *    Either 'camera' or 'library'
+    * @return {Promise}
+    *    Resolves with a {File}
+    */
+   _getPicture(type="camera") {
+      return new Promise((resolve, reject) => {
+         // Get new picture from device camera
+         if (type == 'camera') {
+            this._$input.attr('capture', 'camera');
+         }
+         // Get picture from device photo album
+         else {
+            this._$input.removeAttr('capture');
+         }
+         
+         // Enable
+         let isCameraActive = true;
+         $('body').append(this._$backend);
+
+         // Event handling
+         this._$input.once('change', () => {
+            if (isCameraActive) {
+               isCameraActive = false;
+               let file = this._$input.get(0).files[0];
+               // A photo was captured
+               if (file) {
+                  resolve(file);
+               }
+               // Sometimes the 'change' event triggers on a cancel
+               else {
+                  reject(new Error('Canceled'));
+               }
+            }
+         });
+         $(window).once('focus', () => {
+            // This 'focus' event fires after the camera dialog closes and
+            // the original page gets focus again.
+            setTimeout(() => {
+               // Clear file list
+               this._$reset.trigger('click');
+               // Clean up DOM
+               this._$backend.remove();
+               // If cancel happened with no 'change' event we will catch it here
+               if (isCameraActive) {
+                  isCameraActive = false;
+                  reject(new Error('Canceled'));
+               }
+            }, 300);
+         });
+
+         // Activate the device camera
+         this._$input.trigger('click');
+      });
+   }
+
+
+   init() {
+      // Hidden HTML elements used to trigger camera in _getPicture()
+      this._$backend = $(`
+         <form style="
+               display: block;
+               position: absolute;
+               bottom: 0;
+               right: 0;
+               visibility: hidden;
+         ">
+            <input type="file" accept="image/jpeg,image/jpg,image/gif,image/png" capture="camera" />
+            <input type="reset" />
+         </form>
+      `);
+      this._$input = this.$backend.find("input[type='file']");
+      this._$reset = this.$backend.find("input[type='reset']");
+   }
+
+   //////
+   //  Camera
+   //////
+
+   /**
+    * Activate the camera for the user, and obtain the photo taken.
+    *
+    * @param {int} width
+    * @param {int} height
+    * @return {Promise}
+    *    Resolves with metadata of the saved photo.
+    *    {
+    *       filename: <string>,
+    *       fileEntry: <File|Blob>,
+    *       url: <string> // only valid for current session
+    *    }
+    */
+   getCameraPhoto(width = defaultWidth, height = defaultHeight) {
+      return new Promise((resolve, reject) => {
+         let filename = null;
+         let fileEntry = null;
+         let url = null;
+
+         this._getPicture('camera')
+            .then((file) => {
+               filename = file.name + '-' + uuid();
+               fileEntry = file;
+               url = URL.createObjectURL(file);
+               return fileStorage.put(filename, file);
+            })
+            .then(() => {
+               resolve({
+                  filename,
+                  fileEntry,
+                  url
+               });
+            })
+            .catch((err) => {
+               if (err.message == "Canceled") {
+                  // User canceled the photo. Not a real error.
+                  reject(err);
+               }
+               else {
+                  Log("CameraPWA:getCameraPhoto():Error", err);
+                  reject(err);
+               }
+            });
+      });
+   }
+
+   /**
+    * Prompt the user to select an existing photo from their library, and
+    * obtain a copy of the chosen photo.
+    *
+    * @param {int} width
+    * @param {int} height
+    * @return {Promise}
+    *    Resolves with metadata of the copied photo.
+    *    {
+    *       filename: <string>,
+    *       fileEntry: <File|Blob>,
+    *       url: <string> // only valid for current session
+    *    }
+    */
+   getLibraryPhoto(width = defaultWidth, height = defaultHeight) {
+      return new Promise((resolve, reject) => {
+         let filename = null;
+         let fileEntry = null;
+         let url = null;
+
+         this._getPicture('library')
+            .then((file) => {
+               filename = file.name + '-' + uuid();
+               fileEntry = file;
+               url = URL.createObjectURL(file);
+               return fileStorage.put(filename, file);
+            })
+            .then(() => {
+               resolve({
+                  filename,
+                  fileEntry,
+                  url
+               });
+            })
+            .catch((err) => {
+               if (err.message == "Canceled") {
+                  // User canceled the photo. Not a real error.
+                  reject(err);
+               }
+               else {
+                  Log("CameraPWA:getCameraPhoto():Error", err);
+                  reject(err);
+               }
+            });
+      });
+   }
+
+   ////////
+   // Photo file management
+   ////////
+
+   /**
+    * Copies an existing image into the temp directory and delivers the 
+    * URL for it.
+    *
+    * This is no longer necessary under PWA.
+    * 
+    * @param {string|FileEntry} imageFile
+    *      Either a string filename, or a FileEntry object for this image
+    *      file.
+    * @return {Promise}
+    *      Resolves with the URL to the temp image.
+    */
+   tempUrl(imageFile) {
+      return Promise.resolve()
+         .then(() => {
+            if (imageFile instanceof FileEntry || imageFile instanceof File) {
+               return imageFile;
+            }
+            else if (typeof imageFile == 'string') {
+               return this.loadPhotoByName(imageFile)
+                  .then((image) => {
+                     return image.fileEntry;
+                  })
+            }
+            else {
+               throw new TypeError();
+            }
+         })
+         .then((fileEntry) => {
+            let url = URL.createObjectURL(fileEntry);
+            return url;
+         });
+   }
+
+
+   /**
+    * Remove a photo by its filename.
+    *
+    * @param {string} photoName
+    * @return {Promise}
+    */
+   deletePhoto(photoName) {
+      return fileStorage.delete(photoName);
+   }
+
+
+   /**
+    *  Remove all locally saved photos
+    * @return {Promise}
+    */
+   deleteLocalImages() {
+      return fileStorage.deleteAll();
+   }
+
+
+   /**
+    * TODO: find out what this is used for and refactor it for PWA
+    */
+   imageCleanUp() {
+      return new Promise((resolve, reject) => {
+         // make sure _testDirectoryEntry is created before trying to use:
+         if (!this.directoryEntry) {
+            this.init().then(() => {
+               this.imageCleanUp()
+                  .then((data) => {
+                     resolve(data);
+                  })
+                  .catch(reject);
+            });
+            return;
+         }
+
+         // Get a directory reader
+         var directoryReader = this.directoryEntry.createReader();
+         // Get a list of all the entries in the directory
+         directoryReader.readEntries(
+            (entries) => {
+               var currentDate = new Date();
+               var currentTime = currentDate.getTime();
+               entries.forEach((item, i) => {
+                  if (item.isFile && item.name.indexOf("receipt-") > -1) {
+                     item.getMetadata(
+                        (file) => {
+                           var timeDiff = Math.abs(
+                              currentTime - file.modificationTime.getTime()
+                           );
+                           var diff = Math.ceil(timeDiff / (1000 * 3600 * 24));
+                           if (diff > 14) {
+                              item.remove(
+                                 function() {
+                                    console.log("File removed");
+                                    if (item.name.indexOf("receipt-") > -1) {
+                                       storage.set(
+                                          "Receipt Image-" +
+                                             item.name
+                                                .replace("receipt-", "")
+                                                .replace(".jpg", ""),
+                                          null
+                                       );
+                                    }
+                                 },
+                                 function() {
+                                    console.log("Error while removing file");
+                                 }
+                              );
+                           }
+                        },
+                        (error) => {
+                           reject(error);
+                        }
+                     );
+                  }
+               });
+            },
+            (error) => {
+               reject("Failed during operations: " + error.code);
+            }
+         );
+      });
+   }
+
+
+   /**
+    * Calculate the total size of all locally saved images?
+    * @return {Promise}
+    *    Resolves with {int} total storage in MB
+    */
+   imageLookUp() {
+      return fileStorage.getTotalSize()
+         .then((sizeInBytes) => {
+            sizeInBytes = sizeInBytes || 0;
+            let sizeInMegabytes = Number(sizeInBytes / 1024 / 1024).toFixed(2);
+            console.log("Total Storage: " + sizeInMegabytes);
+            return sizeInMegabytes;
+         });
+   }
+
+
+   /**
+    * Loads a previously saved photo by its filename.
+    *
+    * @param {string} filename
+    * @return {Promise}
+    *    {
+    *       filename: <string>,
+    *       fileEntry: <FileEntry>,
+    *       url: <string> // only valid for current session
+    *    }
+    */
+   loadPhotoByName(filename) {
+      return new Promise((resolve, reject) => {
+         fileStorage.get(filename)
+            .then((imageFile) => {
+               let url = URL.createObjectURL(imageFile);
+               resolve({
+                  filename: filename,
+                  fileEntry: imageFile,
+                  url: url
+               });
+            })
+            .catch((err) => {
+               Log("Unable to find photo file", err);
+               reject(err);
+            })
+      });
+   }
+
+
+   /**
+    * Read the given file's data and deliver it as a base64 ascii string.
+    *
+    * @param {string} name
+    *      Filename
+    * @return {Promise}
+    *      Resolves with {string}
+    */
+   base64ByName(name) {
+      return new Promise((resolve, reject) => {
+         fileStorage.get(name)
+            .then((file) => {
+               // Read the file's data
+               var reader = new FileReader();
+               reader.onloadend = function() {
+                  var binary = this.result;
+                  var base64 = window.btoa(binary);
+                  resolve(base64);
+               };
+               reader.readAsBinaryString(file);
+            })
+            .catch(reject);
+      });
+   }
+
+
+   /**
+    * Read the given file's data and deliver it as a Blob.
+    *
+    * @param {string} name
+    *      Filename
+    * @return {Promise}
+    *      Resolves with {Blob|File}
+    */
+   blobByName(name) {
+      return fileStorage.get(name);
+   }
+
+
+   /**
+    * DEPRECATED
+    * 
+    * Assigning an existing photo file a new name within the same directory.
+    *
+    * @param {string} fromName
+    * @param {string} toName
+    * @return {Promise}
+    */
+   rename(fromName, toName) {
+      throw new Error("rename() is deprecated");
+   }
+   
+
+    /**
+     * Convert a base64 string in a Blob according to the data and contentType.
+     * 
+     * @param b64Data {String} Pure base64 string without contentType
+     * @param contentType {String} the content type of the file i.e (image/jpeg - image/png - text/plain)
+     * @param sliceSize {Int} SliceSize to process the byteCharacters
+     * @see http://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+     * @return Blob
+     */
+    b64toBlob(b64Data, contentType, sliceSize) {
+            contentType = contentType || '';
+            sliceSize = sliceSize || 512;
+
+            var byteCharacters = atob(b64Data);
+            var byteArrays = [];
+
+            for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+                var slice = byteCharacters.slice(offset, offset + sliceSize);
+
+                var byteNumbers = new Array(slice.length);
+                for (var i = 0; i < slice.length; i++) {
+                    byteNumbers[i] = slice.charCodeAt(i);
+                }
+
+                var byteArray = new Uint8Array(byteNumbers);
+
+                byteArrays.push(byteArray);
+            }
+
+          var blob = new Blob(byteArrays, {type: contentType});
+          return blob;
+    }
+
+
+   /**
+    * Save binary data to a file.
+    *
+    * @param {Blob?} data
+    * @param {string} filename
+    * @param {string} [mimeType]
+    * @return {Promise}
+    */
+   saveBinaryToName(data, filename, mimeType=null) {
+      let file;
+      if (!mimeType) {
+         file = new File([data], filename);
+      }
+      else {
+         file = new File([data], filename, { type: mimeType });
+      }
+      let url = URL.createObjectURL(file);
+
+      return fileStorage.put(filename, file)
+         .then(() => {
+            return {
+               filename: filename,
+               fileEntry: file,
+               url: url
+            }
+         });
+   }
+
+}
+
+export default CameraPlatform;
---
+++ resources/FileStorage.js
@@ -0,0 +1,232 @@
+import EventEmitter from 'eventemitter2';
+import uuid from 'uuid/v1';
+import { storage, Storage } from './Storage.js';
+import analytics from './Analytics.js';
+import Log from './Log.js';
+
+const storeName = "file_data";
+
+/**
+ * Uses an IndexedDB backend to store file blobs. Encryption is not currently
+ * supported, but is possible in principle. One difference between this and the
+ * other storage engine (Storage.js) is that this does not serialize the stored
+ * values into JSON. It would be too memory intensive to serialze large files
+ * this way.
+ */
+class FileStorage extends EventEmitter {
+   constructor(name='file_blobs') {
+      super();
+
+      try {
+         var request = indexedDB.open(name);
+         request.onerror = (event) => {
+            Log("FileStorage IndexedDB failure on init", request.error);
+            analytics.logError(request.error);
+         }
+         request.onsuccess = (event) => {
+            this.db = request.result;
+            this.db.onerror = (event) => {
+               Log("FileStorage IndexedDB error", event.target.errorCode);
+            }
+            // Test if the object store is present
+            var transaction = this.db.transaction(storeName, "readonly");
+            transaction.onerror = (event) => {
+               console.log('FileStorage IndexedDB store not found?', transaction.error);
+               // Store was not found. Try to create it now.
+               if (transaction.error.name == "NotFoundError") {
+                  this.db.createObjectStore(storeName);
+               }
+            }
+         }
+         // On first time, set up the obect store
+         request.onupgradeneeded = (event) => {
+            var db = event.target.result;
+            var objectStore = db.createObjectStore(storeName);
+         }
+      } 
+      // IndexedDB not supported on this device?
+      catch (err) {
+         Log(err);
+         alert(
+            "Error initializing the file storage system:\n" +
+               (err.message || "") +
+               "\n" +
+               (err.stack || "")
+         );
+         analytics.logError(err);
+      }
+   }
+
+
+   /**
+    * Update the internal counter that tracks the total size of all files
+    * being stored.
+    * 
+    * @param {integer} sizeChange
+    * @return {Promise}
+    */
+   _updateTotalSize(sizeChange) {
+      return this.get('__totalSizeOfAllFiles')
+         .then((totalSize) => {
+            totalSize = totalSize || 0;
+            if (totalSize != 0) {
+               totalSize += sizeChange;
+               return this.put('__totalSizeOfAllFiles', totalSize);
+            }
+         });
+   }
+
+
+   /**
+    * Get the total size of all files being stored.
+    * 
+    * @return {Promise}
+    *   Resolves with {int}
+    */
+   getTotalSize() {
+      return this.get('__totalSizeOfAllFiles')
+         .then((totalSize) => {
+            totalSize = totalSize || 0;
+            return totalSize;
+         });
+   }
+
+
+   /**
+    * Save a file under a given name.
+    *
+    * @param {string} name
+    * @param {Blob} data
+    * @return {Promise}
+    */
+   put(name, file) {
+      return new Promise((resolve, reject) => {
+         var transaction = this.db.transaction(storeName, "readwrite");
+         transaction.oncomplete = (event) => {
+            if (file.size) {
+               this._updateTotalSize(file.size);
+            }
+            resolve();
+         }
+         transaction.onerror = (event) => {
+            Log("DB error during put", transaction.error)
+            reject(transaction.error);
+         }
+
+         var store = transaction.objectStore(storeName);
+         var req = store.put(file, name);
+      });
+   }
+
+
+   /**
+    * Retrieve a stored file.
+    *
+    * @param {string} name
+    * @return {Promise}
+    *    Resolves with {File|Blob}
+    */
+   get(name) {
+      return new Promise((resolve, reject) => {
+         // Sometimes the DB may not be initialized in time
+         if (!this.db) {
+            console.warn("FileStorage DB not available yet. Retrying...");
+            setTimeout(() => {
+               this.get(name)
+                  .then(resolve)
+                  .catch(reject);
+            }, 50);
+         }
+         // DB is ready now
+         else {
+            var transaction = this.db.transaction(storeName, "readonly");
+            transaction.onerror = (event) => {
+               Log("FileStorage DB error during get", transaction.error)
+               reject(transaction.error);
+            }
+
+            var store = transaction.objectStore(storeName);
+            var req = store.get(key);
+            req.onsuccess = (event) => {
+               let file = req.result;
+               resolve(file);
+            }
+         }
+      });
+   }
+
+
+   /**
+    * Retrieve the URL of a stored file.
+    *
+    * To prevent memory leaks, you can call URL.revokeObjectURL() on the URL
+    * after you are done with it.
+    *
+    * @param {string} name
+    * @return {Promise}
+    *    Resolves with {string}
+    */
+   getURL(name) {
+      return this.get(name)
+         .then((file) => {
+            return URL.createObjectURL(file);
+         })
+   }
+
+
+   /**
+    * Delete a stored file.
+    *
+    * @param {string} name
+    * @return {Promise}
+    */
+   delete(name) {
+      return new Promise((resolve, reject) => {
+         // Retrieve the stored file first
+         this.get(name)
+            .then((file) => {
+               var fileSize = file.size;
+               var transaction = this.db.transaction(storeName, "readwrite");
+               transaction.onerror = (event) => {
+                  Log("FileStorage DB error during delete", event.error);
+                  analytics.logError(event.error);
+                  reject(event.error);
+               }
+               var store = transaction.objectStore(storeName);
+               var req = store.delete(name);
+               req.onsuccess = (event) => {
+                  this._updateTotalSize(-fileSize);
+                  resolve();
+               }
+            })
+            .catch((err) => {
+               reject(err);
+            });
+      });
+   }
+
+
+   /**
+    * Delete all stored files.
+    *
+    * @return {Promise}
+    */
+   deleteAll() {
+      return new Promise((resolve, reject) => {
+         var transaction = this.db.transaction(storeName, "readwrite");
+         transaction.onerror = (event) => {
+            Log("FileStorage DB error during deleteAll", event.error);
+            analytics.logError(event.error);
+            reject(event.error);
+         }
+         var store = transaction.objectStore(storeName);
+         var req = store.clear();
+         req.onsuccess = (event) => {
+            resolve();
+         }
+      });
+   }
+}
+
+var fileStorage = new FileStorage();
+export default fileStorage;
