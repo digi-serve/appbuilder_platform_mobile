2472a53ad184269daf9747c1fbc594b4e7043b0e
Achoobert <greenace14@gmail.com>
2022-12-01 10:13:01 -0700

    Remove bad error check

--- AppBuilder/platform/ABModelRelay.js
+++ AppBuilder/platform/ABModelRelay.js
@@ -80,9 +80,6 @@ module.exports = class ABModelRelay extends ABModelCore {
     */
    findAll(cond) {
       cond = cond || {};
-      if (!cond.tenantUUID || !cond.username) {
-         console.error("Params has no data", cond);
-      }
 
       // this is where the logic will get tricky:
       // As the platform implementation of .findAll()
