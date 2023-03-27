0e97a2bd5612c86bbd5d8c3a5751249fc3da165f
Isaac Schubert <greenace14@gmail.com>
2023-03-27 15:27:02 +0700

    update, use process env

--- resources/Analytics.js
+++ resources/Analytics.js
@@ -33,7 +33,7 @@ class Analytics extends EventEmitter {
          try {
             console.log("Sentry.io plugin required, now init");
             sentry.init({
-               dsn: config.sentryio.dsn, // "https://9df6fd4623934fadb4a9ee6bb6ec887f@sentry.io/1186956",
+               dsn: process?.env?.SENTRY_DEV_DSN || config.sentryio.dsn, // "https://9df6fd4623934fadb4a9ee6bb6ec887f@sentry.io/1186956",
                debug: true,
                release: VERSION.version || null,
             });
