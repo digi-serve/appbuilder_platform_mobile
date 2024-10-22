df0c8cb178e0189d83b8f6cf379a4282da9e13e5
Isaac Schubert <greenace14@gmail.com>
2024-10-22 09:51:55 +0700

    [add] inactive tracker

--- pages/appPage.js
+++ pages/appPage.js
@@ -367,7 +367,7 @@ class AppPage extends Common {
             e.preventDefault();
 
             app.toast
-               .create({
+               ?.create({
                text: 'Do you want to exit?',
                closeButton: true,
                closeButtonText: 'Exit',
